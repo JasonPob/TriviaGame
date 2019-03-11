2b93b952def131e6ba0ac57dc5b9ce11b58c8577
JasonPob <jasonpob@gmail.com>
2019-03-11 14:22:41 -0400

    html layout made+bootstrap

---
+++ assets/javascript/app.js
@@ -0,0 +1,6 @@
+// var correctAnswers = 0;
+//         var incorrectAnswers = 0;
+//         var unanswered = 0;
+//         $(".answer").on("click", function () {
+//             alert($(this).val())
+//         })
